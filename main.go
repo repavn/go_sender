2dfe02029161c497f8fa129d50540a057bd99f8d
Repavn <repavn@gmail.com>
2020-09-07 16:13:44 +0400

    a

--- main.go
+++ main.go
@@ -4,7 +4,7 @@ package main
 	This is a service of sending message to the messengers (telegramm, email,)
 	TODO:
 	1. Take POST queries with message body, head, file etc.
-	2. After POST query - fast response, and asynchronously send message to email
+	2. After POST query - fast response, and asynchronously send message to email 11
 */
 import "fmt"
 
