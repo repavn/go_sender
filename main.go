2b01e364c5d89a98a0b42e1ddaff5ff04763b17f
Repavn <repavn@gmail.com>
2020-09-07 16:08:13 +0400

    init

---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+/*
+	This is a service of sending message to the messengers (telegramm, email,)
+	TODO:
+	1. Take POST queries with message body, head, file etc.
+	2. After POST query - fast response, and asynchronously send message to email
+*/
+import "fmt"
+
+func main() {
+	fmt.Println("Start")
+}
